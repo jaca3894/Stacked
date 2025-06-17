49f220822889953e73ab95f9cb2288f3b4b23af3
Qczer <119138392+Qczer@users.noreply.github.com>
2025-06-17 16:47:35 +0200

    Updated welcome screen

--- app/WelcomeScreen.tsx
+++ app/WelcomeScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 import type { StackNavigationProp } from '@react-navigation/stack';
 
@@ -8,21 +8,33 @@ type WelcomeScreenProps = {
 
 const WelcomeScreen = ({ navigation }: WelcomeScreenProps) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Welcome</Text>
-      <TouchableOpacity
-        style={{
-          backgroundColor: 'blue',
-          padding: 10,
-          borderRadius: 5,
-          marginTop: 20,
-        }}
-        onPress={() => navigation.navigate('Main tabs')}
-        >
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome</Text>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Main tabs')}>
         <Text style={{ color: 'white', fontSize: 16 }}>Continue</Text>
-        </TouchableOpacity>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1c1c1c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  }
+})
 
 export default WelcomeScreen;
